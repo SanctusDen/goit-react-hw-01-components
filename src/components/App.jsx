9a7218daf86a5f60a6b648034e8028343c010081
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-07-04 01:18:24 +0200

    com

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,39 @@
-export const App = () => {
+import user from '../Profile/user.json';
+import data from '../Statistics/data.json';
+import friends from '../FriendList/friends.json';
+// import transactions from '../Transactions/transactions.json';
+
+import Profile from './Profile';
+import Statistics from './Statistics';
+import FriendList from './FriendList';
+// import Transactions from './Transactions';
+
+export function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      
+      <div>
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
+      </div>
+
+      <div>
+        <FriendList friends={friends} />;
+      </div>
+
+      {/* <div>
+        <TransactionHistory items={transactions} />;
+      </div> */}
+      
     </div>
   );
 };
